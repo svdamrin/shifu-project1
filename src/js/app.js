0ed8c2abeb0564ea2b47f22229c5151023b0747f
svdamrin <s.damrin@metro64.ru>
2018-06-29 13:28:06 +0300

    shfu-project1-ex4-light1

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './router';
 import jsonData from '../data/list.json'
-
+import 'bootstrap';
+import '../scss/main.scss'
 
 localStorage.setItem("myJsonData", JSON.stringify(jsonData));
+
 
 
 ReactDOM.render(
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
@@ -14,21 +15,23 @@ module.exports = {
          contentBase: './dist'
        },
   plugins: [
+    new ExtractTextPlugin({filename: 'style.css'}),
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-          template: './index.html',
-           title: 'Output Management'
-         })
+      template: './index.html',
+      title: 'Output Management'
+    })
        ],
   module: {
          rules: [
-           {
-             test: /\.css$/,
-             use: [
-               'style-loader',
-               'css-loader'
-             ]
-           },
+          {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract(
+              {
+                fallback: 'style-loader',
+                use: ['css-loader', 'sass-loader']
+              })
+          },  
            { test: /\.js$/, 
             exclude: /node_modules/, 
             loader: "babel-loader" 
